51cc30d42ba728a54c1fd149c65f8744335a3d0c
charlidsun <sunjingge@outlook.com>
2018-09-03 18:19:01 +0800

    客户端

---
+++ heartclient/src/main/java/com/sun/heartclient/HeartclientApplication.java
@@ -0,0 +1,12 @@
+package com.sun.heartclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartclientApplication.class, args);
+	}
+}
